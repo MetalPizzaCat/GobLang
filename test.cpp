354f530663c5932c187ea5ded2d9caa6f09bf92d
metalpizzacat <catguy228@gmail.com>
2025-02-03 15:30:50 +1000

    add basic block validation

--- test.cpp
+++ test.cpp
@@ -4,16 +4,29 @@
 #include "compiler/Parser.hpp"
 #include "compiler/Validator.hpp"
 
+bool f1()
+{
+    std::cout << "f1" << std::endl;
+    return false;
+}
+
+bool f2()
+{
+    std::cout << "f2" << std::endl;
+    return false;
+}
 int main(int, char **)
 {
     using namespace GobLang::Compiler;
-    Parser p("c = a + (a - 5) / g;");
+
+    bool b = f1() || f2();
+    // {let c = a + (3 - 0); let g = wawa; wawa = (w / 2);}
+    Parser p("{let c = a + (3 - 0); let g = wawa; wawa = (w / 2);}");
     p.parse();
     p.printCode();
     Validator v(p);
     Validator::TokenIterator endIt;
-    assert(v.id(p.getTokens().begin()));
-    assert(v.assignment(p.getTokens().begin(), endIt));
+    assert(v.block(p.getTokens().begin(), endIt));
 
     return EXIT_SUCCESS;
 }
